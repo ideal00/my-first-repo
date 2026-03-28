6e78e4eaa54fa53137123d5943e4ad1a643cd274
ideal00 <jsjs1005@qq.com>
2026-03-28 23:36:28 +0800

    gx

--- 电话号码组合.cpp
+++ 电话号码组合.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
+        vector<> res;
+        for(char c : digits) {
+            int num = c - '1';
+            res.push_back(num);
+        }
+        for(int i : res) {
+            ;
+        }
         
     }
 };
+int main(){
+    int a = 98;
+    char c = a;
+    cout<<c<<endl;
+    
+}//97 98 99  100 101 102  103 104 105  106 107 108  109 110 111  112 113 114  115 116 117 118 119 120 121 122
